a2b46c1dce55bf3a395c56c7eb49aa6742d79a36
Rails Course <krupalipanchal1995@gmail.com>
2019-09-20 18:07:43 +0530

    extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -44,6 +45,9 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
